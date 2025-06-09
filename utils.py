f890da7798d5971c6e52839b0ef571b21dcb5771
Zhao99m <167290705+Zhao99m@users.noreply.github.com>
2025-06-09 09:18:29 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,244 +8,39 @@ import torchvision
 import torchvision.utils as v_utils
 import matplotlib.pyplot as plt
 import cv2
+import pandas as pd
 import math
 from collections import OrderedDict
 import copy
 import time
 from sklearn.metrics import roc_auc_score
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+def filter(data, template, radius=5):
+    arr=np.array(data)
+    length=arr.shape[0]  
+    newData=np.zeros(length) 
 
+    for j in range(radius//2,arr.shape[0]-radius//2):
+        t=arr[ j-radius//2:j+radius//2+1]
+        a=np.multiply(t,template)
+        newData[j]=a.sum()
+    # expand
+    for i in range(radius//2):
+        newData[i]=newData[radius//2]
+    for i in range(-radius//2,0):
+        newData[i]=newData[-radius//2]    
+    # import pdb;pdb.set_trace()
+    return newData
 
-class SmoothedValue(object):
-    """Track a series of values and provide access to smoothed values over a
-    window or the global series average.
-    """
-
-    def __init__(self, window_size=20, fmt=None):
-        if fmt is None:
-            fmt = "{median:.4f} ({global_avg:.4f})"
-        self.deque = deque(maxlen=window_size)
-        self.total = 0.0
-        self.count = 0
-        self.fmt = fmt
-
-    def update(self, value, n=1):
-        self.deque.append(value)
-        self.count += n
-        self.total += value * n
-
-    def synchronize_between_processes(self):
-        """
-        Warning: does not synchronize the deque!
-        """
-        if not is_dist_avail_and_initialized():
-            return
-        t = torch.tensor([self.count, self.total], dtype=torch.float64, device='cuda')
-        dist.barrier()
-        dist.all_reduce(t)
-        t = t.tolist()
-        self.count = int(t[0])
-        self.total = t[1]
-
-    @property
-    def median(self):
-        d = torch.tensor(list(self.deque))
-        return d.median().item()
-
-    @property
-    def avg(self):
-        d = torch.tensor(list(self.deque), dtype=torch.float32)
-        return d.mean().item()
-
-    @property
-    def global_avg(self):
-        return self.total / self.count
-
-    @property
-    def max(self):
-        return max(self.deque)
-
-    @property
-    def value(self):
-        return self.deque[-1]
-
-    def __str__(self):
-        return self.fmt.format(
-            median=self.median,
-            avg=self.avg,
-            global_avg=self.global_avg,
-            max=self.max,
-            value=self.value)
-
-
-class MetricLogger(object):
-    def __init__(self, delimiter="\t"):
-        self.meters = defaultdict(SmoothedValue)
-        self.delimiter = delimiter
-
-    def update(self, **kwargs):
-        for k, v in kwargs.items():
-            if isinstance(v, torch.Tensor):
-                v = v.item()
-            assert isinstance(v, (float, int))
-            self.meters[k].update(v)
-
-    def __getattr__(self, attr):
-        if attr in self.meters:
-            return self.meters[attr]
-        if attr in self.__dict__:
-            return self.__dict__[attr]
-        raise AttributeError("'{}' object has no attribute '{}'".format(
-            type(self).__name__, attr))
-
-    def __str__(self):
-        loss_str = []
-        for name, meter in self.meters.items():
-            loss_str.append(
-                "{}: {}".format(name, str(meter))
-            )
-        return self.delimiter.join(loss_str)
-
-    def synchronize_between_processes(self):
-        for meter in self.meters.values():
-            meter.synchronize_between_processes()
-
-    def add_meter(self, name, meter):
-        self.meters[name] = meter
-
-    def log_every(self, iterable, print_freq, header=None):
-        i = 0
-        if not header:
-            header = ''
-        start_time = time.time()
-        end = time.time()
-        iter_time = SmoothedValue(fmt='{avg:.4f}')
-        data_time = SmoothedValue(fmt='{avg:.4f}')
-        space_fmt = ':' + str(len(str(len(iterable)))) + 'd'
-        log_msg = [
-            header,
-            '[{0' + space_fmt + '}/{1}]',
-            'eta: {eta}',
-            '{meters}',
-            'time: {time}',
-            'data: {data}'
-        ]
-        if torch.cuda.is_available():
-            log_msg.append('max mem: {memory:.0f}')
-        log_msg = self.delimiter.join(log_msg)
-        MB = 1024.0 * 1024.0
-        for obj in iterable:
-            data_time.update(time.time() - end)
-            yield obj
-            iter_time.update(time.time() - end)
-            if i % print_freq == 0 or i == len(iterable) - 1:
-                eta_seconds = iter_time.global_avg * (len(iterable) - i)
-                eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
-                if torch.cuda.is_available():
-                    print(log_msg.format(
-                        i, len(iterable), eta=eta_string,
-                        meters=str(self),
-                        time=str(iter_time), data=str(data_time),
-                        memory=torch.cuda.max_memory_allocated() / MB))
-                else:
-                    print(log_msg.format(
-                        i, len(iterable), eta=eta_string,
-                        meters=str(self),
-                        time=str(iter_time), data=str(data_time)))
-            i += 1
-            end = time.time()
-        total_time = time.time() - start_time
-        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-        print('{} Total time: {} ({:.4f} s / it)'.format(
-            header, total_time_str, total_time / len(iterable)))
-
-
-def _load_checkpoint_for_ema(model_ema, checkpoint):
-    """
-    Workaround for ModelEma._load_checkpoint to accept an already-loaded object
-    """
-    mem_file = io.BytesIO()
-    torch.save(checkpoint, mem_file)
-    mem_file.seek(0)
-    model_ema._load_checkpoint(mem_file)
-
-
-def setup_for_distributed(is_master):
-    """
-    This function disables printing when not in master process
-    """
-    import builtins as __builtin__
-    builtin_print = __builtin__.print
-
-    def print(*args, **kwargs):
-        force = kwargs.pop('force', False)
-        if is_master or force:
-            builtin_print(*args, **kwargs)
-
-    __builtin__.print = print
-
-
-def is_dist_avail_and_initialized():
-    if not dist.is_available():
-        return False
-    if not dist.is_initialized():
-        return False
-    return True
-
-
-def get_world_size():
-    if not is_dist_avail_and_initialized():
-        return 1
-    return dist.get_world_size()
-
-
-def get_rank():
-    if not is_dist_avail_and_initialized():
-        return 0
-    return dist.get_rank()
-
-
-def is_main_process():
-    return get_rank() == 0
-
-
-def save_on_master(*args, **kwargs):
-    if is_main_process():
-        torch.save(*args, **kwargs)
-
-
-def init_distributed_mode(args):
-    if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
-        args.rank = int(os.environ["RANK"])
-        args.world_size = int(os.environ['WORLD_SIZE'])
-        args.gpu = int(os.environ['LOCAL_RANK'])
-    elif 'SLURM_PROCID' in os.environ:
-        args.rank = int(os.environ['SLURM_PROCID'])
-        args.gpu = args.rank % torch.cuda.device_count()
-    else:
-        print('Not using distributed mode')
-        args.distributed = False
-        return
-
-    args.distributed = True
-
-    torch.cuda.set_device(args.gpu)
-    args.dist_backend = 'nccl'
-    print('| distributed init (rank {}): {}'.format(
-        args.rank, args.dist_url), flush=True)
-    torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
-                                         world_size=args.world_size, rank=args.rank)
-    torch.distributed.barrier()
-    setup_for_distributed(args.rank == 0)
-
-
-def update_from_config(args):
-    cfg = mmcv.Config.fromfile(args.config)
-    for _, cfg_item in cfg._cfg_dict.items():
-        for k, v in cfg_item.items():
-            setattr(args, k, v)
-    return args
-
+def calc(r=5, sigma=2):
+    k = np.zeros(r)
+    for i in range(r):
+        k[i] = 1/((2*math.pi)**0.5*sigma)*math.exp(-((i-r//2)**2/2/(sigma**2)))
+    return k
 
 def rmse(predictions, targets):
     return np.sqrt(((predictions - targets) ** 2).mean())
@@ -258,6 +53,19 @@ def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
 
+def dismap(x, name='dismap'):
+    # import pdb;pdb.set_trace()
+    x = x.data.cpu().numpy()
+    x = x.mean(1)
+    for j in range(x.shape[0]):
+        plt.cla()
+        y = x[j]
+        # import pdb;pdb.set_trace()
+        df = pd.DataFrame(y)
+        sns.heatmap(df)
+        plt.savefig('results/dismap/{}_{}.png'.format(name,str(j)))
+        plt.close()
+    return True
 
 def normalize_img(img):
 
@@ -305,3 +113,119 @@ def score_sum(list1, list2, alpha):
         list_result.append((alpha*list1[i]+(1-alpha)*list2[i]))
         
     return list_result
+
+def moving_average(interval, windowsize):
+    window = np.ones(int(windowsize)) / float(windowsize)
+    re = np.convolve(interval, window, 'same')
+    return re
+
+def draw_score_curve(aa, bb, cc, cur_gt, name='results/curves_pt', vid = ''):
+    
+    T = range(len(aa))
+    xnew = np.linspace(0,len(aa),10*len(aa))
+    aa_new = 1-np.array(aa)
+    aa_new = moving_average(aa_new,5)
+    bb_new = 1-np.array(bb)
+    bb_new = moving_average(bb_new,5)
+    cc_new = 1-np.array(cc)
+    cc_new = moving_average(cc_new,5)
+    # cur_gt = make_interp_spline(T, cur_gt)(xnew)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
+    cur_ans = cc_new
+    # print(vid)
+    # import pdb;pdb.set_trace()
+    ax1.plot(cur_gt, color='r')
+    ax2.plot(cur_ans, color='g')
+    plt.title(vid)
+    plt.show()
+    plt.savefig(name+'/'+vid+'_all.png')
+    # print('Save: ',root +'/'+vid+'.png')
+    plt.close()
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
+    cur_ans = aa_new
+    # print(vid)
+    # import pdb;pdb.set_trace()
+    ax1.plot(cur_gt, color='r')
+    ax2.plot(cur_ans, color='g')
+    plt.title(vid)
+    plt.show()
+    plt.savefig(name+'/'+vid+'_fra.png')
+    # print('Save: ',root +'/'+vid+'.png')
+    plt.close()
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212)
+    cur_ans = bb_new
+    # print(vid)
+    # import pdb;pdb.set_trace()
+    ax1.plot(cur_gt, color='r')
+    ax2.plot(cur_ans, color='g')
+    plt.title(vid)
+    plt.show()
+    plt.savefig(name+'/'+vid+'_fea.png')
+    # print('Save: ',root +'/'+vid+'.png')
+    plt.close()
+    # import pdb;pdb.set_trace()
+    return True
+
+def depict(videos_list, psnr_list, feature_distance_list, labels_list, root='results/AUCs'):
+    video_num = 0
+    label_length = 0
+    import pdb
+    for video in sorted(videos_list):
+        video_name = video.split('/')[-1]
+        start = label_length
+        end = label_length + len(psnr_list[video_name])
+        # anomaly_score_total_list = score_sum(anomaly_score_list(psnr_list[video_name]), 
+        #                                  anomaly_score_list_inv(feature_distance_list[video_name]), args.alpha)
+        anomaly_score_ae_list = np.asarray(anomaly_score_list(psnr_list[video_name]))
+        anomaly_score_mem_list = np.asarray(anomaly_score_list_inv(feature_distance_list[video_name]))
+        if (1-labels_list[start:end]).max() <1 or (1-labels_list[start:end]).min()==1:
+            accuracy_ae = accuracy_me = 0
+        else:
+            accuracy_ae = AUC(anomaly_score_ae_list, np.expand_dims(1-labels_list[start:end], 0))
+            accuracy_me = AUC(anomaly_score_mem_list, np.expand_dims(1-labels_list[start:end], 0))
+        assert len(labels_list[start:end])==len(anomaly_score_ae_list)
+        # pdb.set_trace()
+        label_length = end
+        fig = plt.figure()
+        ax1 = fig.add_subplot(311)
+        ax2 = fig.add_subplot(312)
+        ax3 = fig.add_subplot(313)
+        #print vid, tf_idf
+        ax1.plot(1-labels_list[start:end], color='r')
+        ax2.plot(anomaly_score_ae_list, color='g')
+        ax3.plot(anomaly_score_mem_list, color='b')
+        plt.title(video_name+' {:.4f} {:.4f}'.format(accuracy_ae, accuracy_me), y=3.4)
+        plt.show()
+        if not os.path.exists(root):
+            os.makedirs(root)
+        plt.savefig(root+'/'+video_name+'.png')
+        # print('Save: ',root +'/'+vid+'.png')
+        plt.close()
+    # pdb.set_trace()
+    return True
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
